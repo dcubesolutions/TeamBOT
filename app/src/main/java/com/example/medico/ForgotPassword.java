d28d89cce97274f7e7857d6f379268d8d9ae4a0f
Meet <meetvora1998@gmail.com>
2019-02-16 19:31:21 +0530

    Committed missing files and remove the build folder from the source

---
+++ app/src/main/java/com/example/medico/ForgotPassword.java
@@ -0,0 +1,50 @@
+package com.example.medico;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+EditText forgotEmail;
+Button forgotBtnConfirm;
+ProgressBar pbar;
+FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        forgotBtnConfirm=(Button) findViewById(R.id.forgotBtnConfirm);
+        forgotEmail= (EditText)findViewById(R.id.forgotEmail);
+        pbar = findViewById(R.id.pbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        forgotBtnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.sendPasswordResetEmail(forgotEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ForgotPassword.this, "password sent to your email address", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(ForgotPassword.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
